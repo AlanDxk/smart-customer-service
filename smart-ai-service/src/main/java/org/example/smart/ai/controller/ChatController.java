419b6802ac40224c2fcf6b4cb2bc0a301c0cab08
AlanDxk <15812936578@163.com>
2026-04-21 23:07:58 +0800

    新增ai交流测试类

---
+++ smart-ai-service/src/main/java/org/example/smart/ai/controller/ChatController.java
@@ -0,0 +1,23 @@
+package org.example.smart.ai.controller;
+
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "聊天接口", description = "提供AI对话功能")
+public class ChatController {
+    @Resource
+    private OpenAiChatModel model;
+    @GetMapping("/chat")
+    @Operation(summary = "测试聊天", description = "测试AI对话功能")
+    public String chat(@RequestParam("message")  String message){
+        String result = model.chat(message);
+        return result;
+    }
+}
